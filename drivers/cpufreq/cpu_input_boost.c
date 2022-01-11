cyberknight777/dragonheart_kernel_oneplus_sm8150

cpu_input_boost: Allow configuration of sm8150's prime cluster's minimum frequency
Considering the fact we're configuring the min freq of LP and HP cluster, it would make perfect sense to configure the prime cluster of sm8150 aswell.

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

drivers/cpufreq/cpu_input_boost.c
@@ -79,8 +79,10 @@ static unsigned int get_min_freq(struct cpufreq_policy *policy)
 
   if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
     freq = CONFIG_REMOVE_INPUT_BOOST_FREQ_LP;
-  else
+  else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
     freq = CONFIG_REMOVE_INPUT_BOOST_FREQ_PERF;
+  else
+    freq = CONFIG_REMOVE_INPUT_BOOST_FREQ_PERFP;
   return max(freq, policy->cpuinfo.min_freq);
 }
 

Atom-X-Devs/android_kernel_xiaomi_raphael

drivers/cpufreq/cpu_input_boost.c
@@ -79,8 +79,10 @@ static unsigned int get_min_freq(struct cpufreq_policy *policy)
 
   if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
     freq = CONFIG_MIN_FREQ_LP;
-  else
+  else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
     freq = CONFIG_MIN_FREQ_PERF;
+  else
+    freq = CONFIG_MIN_FREQ_PERFP;
   return max(freq, policy->cpuinfo.min_freq);
 }
 